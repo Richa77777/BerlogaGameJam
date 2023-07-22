d31c51d351bc956c5d1273750a05cda6cc030355
AlekseyKosarev <alekseykosarev205@gmail.com>
2023-07-22 20:17:14 +0500

    konami code

---
+++ Assets/Core/Aleksey/KonamiCode.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KonamiCode : MonoBehaviour
+{
+    [SerializeField] private List<ButtonDevice> buttons;
+    [SerializeField] private GameObject effect;
+    private List<string> _typeSequence = new List<string>();
+    private List<string> _needSequence = new List<string>(){"up", "up", "down", "down", "left", "right", "left", "right", "B", "A"};
+
+    private void Start()
+    {
+        foreach (var button in buttons)
+        {
+            button.OnClick += ClickButton;
+        }
+    }
+
+    private void ClickButton(string key)
+    {
+        _typeSequence.Add(key);
+        if(_needSequence[_typeSequence.Count-1] != _typeSequence[_typeSequence.Count-1])
+            _typeSequence.Clear();
+        else if (_typeSequence.Count == _needSequence.Count)
+        {
+            CompleteCode();
+        }
+    }
+
+    private void CompleteCode()
+    {
+        foreach (var button in buttons)
+        {
+            button.OnClick -= ClickButton;
+        }
+        effect.SetActive(true);
+    }
+    
+}
